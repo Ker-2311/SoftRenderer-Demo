fc6db1e8f5def8181cf542e5ddbeb87c48e12a5c
Ker-2311 <274552678@qq.com>
2025-09-15 17:37:30 +0800

    【数据结构】顶点结构优化

--- Renderer/Core/Vertex.h
+++ Renderer/Core/Vertex.h
@@ -3,17 +3,17 @@
 
 class Vertex
 {
-private:
-    Vector3f m_pos = Vector3f(0,0,0);
-    Vector3i m_color = Vector3i(0,0,0);
-    Vector3f m_normal = Vector3f(0,0,0);
-    Vector2f m_uv = Vector2f(0,0);
+public:
+    Vector3f pos = Vector3f(0, 0, 0);
+    Vector3f color = Vector3f(0, 0, 0);
+    Vector3f normal = Vector3f(0, 0, 0);
+    Vector2f uv = Vector2f(0, 0);
 
 public:
     Vertex();
-    Vertex(Vector3f pos) : m_pos(pos) {}
-    Vertex(Vector3f pos, Vector3i color) : m_pos(pos), m_color(color) {}
-    Vertex(Vector3f pos, Vector3i color, Vector3f normal) : m_pos(pos), m_color(color), m_normal(normal) {}
+    Vertex(Vector3f p) : pos(p) {}
+    Vertex(Vector3f p, Vector3f c) : pos(p), color(c) {}
+    Vertex(Vector3f p, Vector3f c, Vector3f normal) : pos(p), color(c), normal(normal) {}
 
-    COLORREF GetColor() { return RGB(m_color.x, m_color.y, m_color.z); }
+    COLORREF GetColorWithColorRef() { return RGB(color.x, color.y, color.z); }
 };
